5d7102299145c832c42dc5538a8e855863311810
Rafin Tanzim <66791918+Rafinuwu@users.noreply.github.com>
2023-07-27 00:24:15 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,39 @@
-var quizData = [
+document.addEventListener("DOMContentLoaded", function () {
+  const quizData = [
     {
-        question: "Toi koi?",
-        options: ["hoem", "dudu", "tomar moaan"],
-        answer: "hoem"
+      question: "Toi koi?",
+      options: ["hoem", "dudu", "tomar moaan"],
+      answer: "hoem",
     },
     {
-        question: "Where did we first meet?",
-        options: ["concert", "orientation", "dudu first day"],
-        answer: "dudu first day"
+      question: "Where did we first meet?",
+      options: ["concert", "orientation", "dudu first day"],
+      answer: "dudu first day",
     },
     {
-        question: "What's my most fav memory of us?",
-        options: ["juju", "gulshan datez", "uttara"],
-        answer: "juju"
-    }
-];
+      question: "What's my most fav memory of us?",
+      options: ["juju", "gulshan datez", "uttara"],
+      answer: "juju",
+    },
+  ];
 
-var quizForm = document.getElementById("quiz-form");
-var resultDiv = document.getElementById("result");
-var scoreSpan = document.getElementById("score");
+  const quizForm = document.getElementById("quiz-form");
+  const resultDiv = document.getElementById("result");
+  const scoreSpan = document.getElementById("score");
 
-quizForm.addEventListener("submit", (event) => {
+  quizForm.addEventListener("submit", function (event) {
     event.preventDefault();
 
     let score = 0;
-    var userAnswers = new FormData(quizForm);
+    const userAnswers = new FormData(quizForm);
 
-    quizData.forEach((question, index) => {
-        var selectedAnswer = userAnswers.get(q${index + 1});
-        if (selectedAnswer === question.answer) {
-            score++;
-        }
+    quizData.forEach(function (question, index) {
+      const selectedAnswer = userAnswers.get(q${index + 1});
+      if (selectedAnswer === question.answer) {
+        score++;
+      }
     });
 
     scoreSpan.innerText = score;
     resultDiv.style.display = "block";
-});
+  });
